64306660f1dbe0352b0faba27eb35ff3376ea936
Cach30verfl0w <cach30verfl0w@gmail.com>
2023-08-11 11:03:04 +0200

    Little improvements and removed deprecation in resolve function on Windows

--- src/windows/resolve.cpp
+++ src/windows/resolve.cpp
@@ -25,27 +25,30 @@ namespace sockslib {
         }
 
         std::string address;
+        std::array<wchar_t, 128> address_buffer {};
+        DWORD size = address_buffer.size();
         switch(record->wType) {
             case DNS_TYPE_A: {
-                IN_ADDR ip_addr;
-                ip_addr.S_un.S_addr = record->Data.A.IpAddress;// NOLINT
-                DnsRecordListFree(record, DnsFreeRecordListDeep);
-                address = inet_ntoa(ip_addr);
+                SOCKADDR_IN addr {};
+                addr.sin_family = AF_INET;
+                addr.sin_addr = *reinterpret_cast<in_addr*>(&record->Data.A.IpAddress);// NOLINT
+
+                if(FAILED(WSAAddressToStringW(reinterpret_cast<LPSOCKADDR>(&addr), sizeof(addr), nullptr,// NOLINT
+                                              address_buffer.data(), &size))) {
+                    return kstd::Error {fmt::format("Unable to format IPv4 Address into literal => {}", get_last_error())};
+                }
                 break;
             }
             case DNS_TYPE_AAAA: {
-                SOCKADDR_IN6 addr;
+                SOCKADDR_IN6 addr {};
                 memset(&addr, 0, sizeof(addr));
                 addr.sin6_family = AF_INET6;
-                addr.sin6_addr = *reinterpret_cast<in_addr6*>(&(record->Data.AAAA.Ip6Address));// NOLINT
+                addr.sin6_addr = *reinterpret_cast<in_addr6*>(&record->Data.AAAA.Ip6Address);// NOLINT
 
-                std::array<wchar_t, 46> address_buffer {};
-                DWORD size = address_buffer.size();
                 if(FAILED(WSAAddressToStringW(reinterpret_cast<LPSOCKADDR>(&addr), sizeof(addr), nullptr,// NOLINT
                                              address_buffer.data(), &size))) {
                     return kstd::Error {fmt::format("Unable to format IPv4 Address into literal => {}", get_last_error())};
                 }
-                address = std::string {address_buffer.begin(), address_buffer.begin() + size - 1};
                 break;
             }
             default: return kstd::Error {"Unable to cleanup WSA => Illegal type of record in response"s};
@@ -56,7 +59,10 @@ namespace sockslib {
         if (!wsa_cleanup_result) {
             return kstd::Error {wsa_cleanup_result.get_error()};
         }
-        return {address};
+
+        // Free DNS record and return address
+        DnsRecordListFree(record, DnsFreeRecordListDeep);
+        return {std::string {address_buffer.begin(), address_buffer.begin() + size - 1}};
     }
 
     auto resolve_address(const std::string domain) noexcept -> kstd::Result<std::string> {
